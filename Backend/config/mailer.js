15bfe77eb80204fe55be206f9a258c1c31e89861
Rahul Raturi <rahulraturi84@gmail.com>
2026-06-10 13:31:37 +0530

    fix

--- Backend/config/mailer.js
+++ Backend/config/mailer.js
@@ -1,17 +1,14 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
+import dotenv from "dotenv";
 
-dns.setDefaultResultOrder("ipv4first");
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
 });
 
 export default transporter;

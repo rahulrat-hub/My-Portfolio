0da1cac2565616823aa5c60eb0039e21afd5d556
Rahul Raturi <rahulraturi84@gmail.com>
2026-06-10 12:31:36 +0530

    fix bugs

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 import myroute from './routes/myinfo.js'
 
@@ -8,7 +7,11 @@ dotenv.config();
 
 const app = express()
 app.use(cors({
-     origin: "https://my-portfolio-omega-roan-99.vercel.app",
+     origin: [
+    "http://localhost:5173",
+    "https://my-portfolio-omega-roan-99.vercel.app",
+    "https://my-portfolio-ih72vtuh7-rahul-r-project.vercel.app"
+  ],
   credentials: true
 }))
 app.use(express.json())

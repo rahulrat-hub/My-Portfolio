c7d50ccacadc16f0b435c52583adf430064f66e0
Rahul Raturi <rahulraturi84@gmail.com>
2026-06-10 21:04:58 +0530

    add mailer

--- Backend/config/mailer.js
+++ Backend/config/mailer.js
@@ -4,11 +4,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+// SMTP Connection Test
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP Server is ready");
+  }
 });
 
 export default transporter;
